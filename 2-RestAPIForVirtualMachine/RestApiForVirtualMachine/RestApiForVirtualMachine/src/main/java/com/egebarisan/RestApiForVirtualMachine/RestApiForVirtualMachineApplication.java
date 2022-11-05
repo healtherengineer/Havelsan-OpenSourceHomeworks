be2ecff10b20cd87c654f84ba03017b8f84af552
Ege Barisan <ege.barisan.01@gmail.com>
2022-11-05 22:06:01 +0300

    started to second task

---
+++ 2-RestAPIForVirtualMachine/RestApiForVirtualMachine/RestApiForVirtualMachine/src/main/java/com/egebarisan/RestApiForVirtualMachine/RestApiForVirtualMachineApplication.java
@@ -0,0 +1,13 @@
+package com.egebarisan.RestApiForVirtualMachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiForVirtualMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiForVirtualMachineApplication.class, args);
+	}
+
+}
---
+++ 2-RestAPIForVirtualMachine/RestApiForVirtualMachine/RestApiForVirtualMachine/src/test/java/com/egebarisan/RestApiForVirtualMachine/RestApiForVirtualMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egebarisan.RestApiForVirtualMachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiForVirtualMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
